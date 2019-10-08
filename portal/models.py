4df448507ebb25dcacb8a12afec31b901c205ddf
Rory O'Connor <roryoconnor@box.com>
2019-10-08 10:01:49 -0500

    Migrate LoanApplication Model

--- portal/models.py
+++ portal/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -12,3 +13,19 @@ class UserProfile(models.Model):
         logging.debug("Trying to create the user profile")
         if created:
             UserProfile.objects.create(user=instance)
+
+class LoanApplication(models.Model):
+	application_id = models.AutoField(primary_key=True)
+	applicant = models.ForeignKey(User, unique=True, on_delete='CASCADE')
+	application_file_id = models.CharField(max_length=20)
+	SUBMITTED = 'SUB'
+	PENDING = 'PEND'
+	APPROVED = 'APP'
+	COMPLETE = 'COMP'
+	status_choices = [(SUBMITTED, 'Submitted'), (PENDING, 'Pending'), (APPROVED, 'Approved'), (COMPLETE, 'Complete')]
+	status = models.CharField(max_length=20, choices=status_choices, default=SUBMITTED)
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+
+	def create_application(self): 
+		self.created_at = timezone.now()
--- portal/views.py
+++ portal/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from portal.forms import UploadFileForm
+from portal.models import LoanApplication
 from boxsdk import OAuth2, Client
 import logging
 from django.contrib import messages
@@ -15,13 +16,15 @@ def index(request):
 @login_required
 def home(request):
     return render(request, 'home.html')
+    logging.debug('We are going to load the page for', user)
 
 def upload_file(request):
+	user = request.user
 	if request.method == 'POST':
 		form = UploadFileForm(request.POST, request.FILES)
 		logging.debug(request, request.POST, 'now', request.FILES)
 		if form.is_valid():
-			handle_uploaded_file(request.FILES['file'], request.POST['first_name'], request.POST['last_name'])
+			handle_uploaded_file(request.FILES['file'], user)
 			logging.debug("This is good", form)
 			return HttpResponseRedirect('/success/url')
 		else:
@@ -32,18 +35,21 @@ def upload_file(request):
 		form = UploadFileForm()
 	return render(request, 'home.html', {'form': form})
 
-def handle_uploaded_file(f, first_name, last_name):
+def handle_uploaded_file(f, user):
 
 	auth = OAuth2(
 	    client_id='ruaf123v1puenhi42ey8qmfyqwd3r7w4',
 	    client_secret='Xc4EMVxss7DStL7CHqO74zKcYgJkfB84',
-	    access_token='mAqSFAATk20JPywPtNgx2r66aysgxaSl',
+	    access_token='dE5kSQabzIoRIOLrVpNiVLHa0CAwNUXZ',
 	)
 	client = Client(auth)
 	stream = f
-	file_name = last_name + ", " + first_name + " Loan Application.pdf"
-	user = client.user().get()
+
+
+	file_name = user.last_name + ", " + user.first_name + " Loan Application.pdf"
 	subfolder = client.folder('0').create_subfolder('My Stuff' + str(random.randint(1,100001)))
 	folder_id = subfolder.id
 	new_file = client.folder(folder_id).upload_stream(f, file_name)
 	logging.debug('File "{0}" uploaded to Box with file ID {1}'.format(new_file.name, new_file.id)) 
+	new_loan = LoanApplication(applicant=user, application_file_id = new_file.id)
+	new_loan.save()
