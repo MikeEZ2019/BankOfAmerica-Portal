f241788efce3a8b0295d9b56283b280a096d0588
Rory O'Connor <roryoconnor@box.com>
2019-10-09 13:02:54 -0500

    update

--- LoanPortal/settings.py
+++ LoanPortal/settings.py
@@ -119,5 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 LOGIN_REDIRECT_URL = '/accounts/home/applications'
 AUTH_PROFILE_MODULE = "account.UserProfile"
