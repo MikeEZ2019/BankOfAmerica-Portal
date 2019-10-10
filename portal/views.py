677c2564e36c01b4a0d23e37aa09321a72951303
Rory O'Connor <roryoconnor@box.com>
2019-10-09 20:36:07 -0500

    webhooks

--- portal/views.py
+++ portal/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.generic import TemplateView
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
