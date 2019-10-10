cc470e17ff3870f149f4a012a6652c6f50fc2d05
Rory O'Connor <roryoconnor@box.com>
2019-10-09 20:41:32 -0500

    webhooks

--- portal/views.py
+++ portal/views.py
@@ -27,7 +27,7 @@ def success(request):
 
 @csrf_exempt
 def handle_webhook(request):
-	logging.debug("Webhook received", request)
+	logging.debug("Webhook received", request.POST)
 	return HttpResponse(status=200)
 
 
