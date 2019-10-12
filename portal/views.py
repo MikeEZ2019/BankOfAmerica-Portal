fb198300daf16143c28d7abf24b76c670560b796
Rory O'Connor <roryoconnor@box.com>
2019-10-12 11:42:13 -0500

    fix success message UX

--- portal/views.py
+++ portal/views.py
@@ -18,7 +18,7 @@ from django.utils import timezone
 auth = OAuth2(
     client_id='ruaf123v1puenhi42ey8qmfyqwd3r7w4',
     client_secret='Xc4EMVxss7DStL7CHqO74zKcYgJkfB84',
-    access_token='BM5RYTZLFoxPahcJlH2yBIAXoU52xuqj',
+    access_token='s1mXvhgeBchkqDHFb8IY168lKcpU6pOc',
 )
 client = Client(auth)
 
